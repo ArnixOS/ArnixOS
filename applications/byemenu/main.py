2133b6dc11f129146fe0728a0a4870e635ab87e7
Abhiraj Rik <97382691+sontaimnt@users.noreply.github.com>
2023-01-01 17:21:02 +0530

    return to legacy powermenu...
    the newer horizontal buttons based byemenu broke everywhere... no system did it perfectly except the old build so we ended up sticking to it...

--- applications/byemenu/main.py
+++ applications/byemenu/main.py
@@ -1,63 +1,25 @@
 from tkinter import *
 from subprocess import check_output
 from os import system
-from os.path import isfile
-from tkinter.messagebox import showerror
 
 self = Tk()
 self.title("arnix-byemenu")
+self.configure(bg="#24283b")
 self.resizable(False , False)
-self.configure(bg="#232a2d")
-self.geometry("500x200")
 
-user = check_output("whoami").decode("utf-8")
+x = int((self.winfo_screenwidth() / 2) - (375/ 2))
+y = int((self.winfo_screenheight() / 2) - (375/ 2))
+self.geometry(f'400x400+{x}+{y}')
 
-canvas = Canvas(self , width=500 , height=200 , bg="#232a2d")
+user = check_output("whoami").decode('utf-8')
+canvas = Canvas(self , width=400 , height=400, bg="#24283b")
 canvas.pack()
 
-def lock():
-    global lockcmd
-    slimpath="/etc/pam.d/slimlock"
-    betterlockscreen="/usr/bin/betterlockscreen"
-    i3lockpath="/usr/bin/i3lock"
-    slockpath="/usr/bin/slock"
-    xflockpath="/usr/bin/xflock4"
-
-    slockexists=isfile(slockpath)
-    i3lockexists=isfile(i3lockpath)
-    betterlockscreenexists=isfile(betterlockscreen)
-    slimexists=isfile(slimpath)
-    xflockexists=isfile(xflockpath)
-
-    if slockexists==True:
-        lockcmd="slock"
-    elif i3lockexists==True:
-        lockcmd="i3lock"
-    elif slimexists==True:
-        lockcmd="slimlock"
-    elif betterlockscreenexists==True:
-        lockcmd="betterlockscreen -l"
-    elif xflockexists==True:
-        lockcmd="xflock4"
-    else:
-        showerror('Error' , 'Please open an issue on https://gitlab.com/arnix-os/byemenu to fix this...')
-        self.destroy()
-
-lock()
-
-b1 = Button(self , text="" , font=("Iosevka 14") , fg="#dadada" , bg="#232a2d" , activebackground="#e57474" , activeforeground="#232a2d" , command=lambda:system("poweroff") , width=3 , height=2)
-b2 = Button(self , text="" , font=("Iosevka 14") , fg="#dadada" , bg="#232a2d" , activebackground="#8ccf7e" , activeforeground="#232a2d" , command=lambda:system("reboot"), width=3, height=2)
-b3 = Button(self , text="" , font=("Iosevka 14") , fg="#dadada" , bg="#232a2d" , activebackground="#e5c76b" , activeforeground="#232a2d" , command=lambda:system(f"{lockcmd}"), width=3, height=2)
-b4 = Button(self , text="ﴚ" , font=("Iosevka 14") , fg="#dadada" , bg="#232a2d" , activebackground="#67b0e8" , activeforeground="#232a2d" , command=lambda:system(f"pkill -U {user}"), width=3, height=2)
-b5 = Button(self , text="" , font=("Iosevka 14") , fg="#dadada" , bg="#232a2d" , activebackground="#c47fd5" , activeforeground="#232a2d" , command=lambda:system("systemctl suspend"), width=3, height=2)
-
-canvas.create_window(250 , 40, window=Label(self , text=f"bye {user}" , font=("Iosevka 12") , fg="#dadada" , bg="#232a2d"))
-canvas.create_window(100 , 100, window=b1)
-canvas.create_window(175 , 100, window=b2)
-canvas.create_window(250 , 100, window=b3)
-canvas.create_window(325 , 100, window=b4)
-canvas.create_window(400 , 100, window=b5)
-
-self.bind("<Escape>" , lambda event:self.destroy())
+canvas.create_window(200 , 50, window=Label(self , text=f"bye , {user}" , bg="#24283b" , fg="#c0caf5" , font=("Caskaydia Cove Nerd Font" , 12)))
+canvas.create_window(200 , 100, window=Button(self , text="⏻ Shut Down" , font=("Caskaydia Cove Nerd Font" , 12) , bg="#7aa2f7" , fg="#24283b" , command=lambda:system("poweroff") , width=10))
+canvas.create_window(200 , 150, window=Button(self , text=" Restart" , font=("Caskaydia Cove Nerd Font" , 12) , bg="#7aa2f7" , fg="#24283b" , command=lambda:system("reboot") , width=10))
+canvas.create_window(200 , 200, window=Button(self , text=" Log Out", font=("Caskaydia Cove Nerd Font" , 12) , bg="#7aa2f7" , fg="#24283b" , command=lambda:system(f"pkill -U {user}") , width=10))
+canvas.create_window(200 , 250, window=Button(self , text="⏾ Suspend" , font=("Caskaydia Cove Nerd Font" , 12) , bg="#7aa2f7" , fg="#24283b" , command=lambda:system("systemctl suspend") , width=10))
+canvas.create_window(200 , 350, window=Button(self , text=" Quit" , font=("Caskaydia Cove Nerd Font" , 12) , bg="#f7768e" , fg="#24283b" , command=lambda:self.destroy() , width=10))
 
 self.mainloop()
