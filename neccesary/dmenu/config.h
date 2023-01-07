9940c1119dd3579b7fae1ac5bd6ee819aa231130
Abhiraj Rik <97382691+sontaimnt@users.noreply.github.com>
2023-01-07 18:19:48 +0530

    Create config.h

---
+++ neccesary/dmenu/config.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"caskaydiacovenerdfontmono:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#c0caf5", "#24283b" },
+	[SchemeSel] = { "#eeeeee", "#0f4b6e" },
+	[SchemeSelHighlight] = { "#7aa2f7"	, "#0f4b6e" },
+	[SchemeNormHighlight] = { "#7aa2f7", "#24283b" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+static unsigned int border_width = 4;
