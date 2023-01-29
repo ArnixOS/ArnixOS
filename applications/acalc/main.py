64859327ae20e9ac70db130352748278082242af
Abhiraj Rik <97382691+sontaimnt@users.noreply.github.com>
2023-01-29 23:22:21 +0530

    Update main.py

--- applications/acalc/main.py
+++ applications/acalc/main.py
@@ -1,5 +1,6 @@
-from tkinter import Text , Label , Button , Tk , Frame
+from tkinter import Text , Label , Button , Tk , Frame , END
 import config
+from calculate import calculate
 
 root = Tk()
 root.title("acalc")
@@ -11,11 +12,12 @@ main.pack()
 f1 = Frame(root , width=45 , height=50 , bg=config.bg)
 f1.pack()
 
-EtButton = Button(f1 , text="Enter" , bg=config.numbers , fg=config.bg , highlightthickness=0)
-EtButton.pack(in_=f1 , side="right")
-
 entry = Text(width=35 , height=1 , bg=config.bg , fg=config.fg , insertbackground=config.fg , highlightthickness=0)
 entry.pack(in_=f1 , side='left')
 
+EtButton = Button(f1 , text="Enter" , bg=config.numbers , fg=config.bg , highlightthickness=0 , width=6 , command=lambda:calculate(entry.get(1.0 , END) , main))
+EtButton.pack(in_=f1 , side="right")
+
+
 if __name__ == "__main__":
-    root.mainloop()
+    root.mainloop()  
